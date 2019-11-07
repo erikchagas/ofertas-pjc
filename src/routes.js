ee627e04cebd4ada762a4012382033eacff2b86f
Erik Chagas <erikchagas00@gmail.com>
2019-11-07 15:29:03 -0400

    Adicionar declaração de Route para Details

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ export default function Routes() {
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Main} />
+        <Route path="/details/:id" component={Details} />
       </Switch>
     </BrowserRouter>
   );
